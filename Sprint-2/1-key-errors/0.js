6a8ca74e7366d7d3e98ced9a5f25af9ce76e4b26
Mohamed Ibrahim Mohamed <mahamedadou@hotmail.com>
2025-06-14 11:17:26 +0100

    Only including sprint-2 changes

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,6 +1,6 @@
 // Predict and explain first...
 //  =============> The following code will result in an error message:
-SyntaxError: It has been established that the identifier "str" has already been declared.
+// SyntaxError: It has been established that the identifier "str" has already been declared.
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
@@ -10,8 +10,8 @@ function capitalise(str) {
 }
 
 // =============> The function capitalize is intended to return the input string with the first character capitalized.
-However, the original version results in a syntax error due to the declaration of str as a function parameter, which precludes its redeclaration using let within the same scope.
-// =============> function capitalise(str) {
+//However, the original version results in a syntax error due to the declaration of str as a function parameter, which precludes its redeclaration using let within the same scope.
+function capitalise(str) {
   str = `${str[0].toUpperCase()}${str.slice(1)}`;
   return str;
 }
